1ee7a026e9434761cbaeb7dd4f0e238d68c2adc5
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-08-14 11:50:12 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+/*placeholder*/
+$(document).ready(function(){
+        $.Placeholder.init({ color : "#6d6d6d" });
+ });
+ 
+ $(document).ready(function(){
+ new WOW().init();
+ 
+ });
+
+
+jQuery(document).ready(function() {
+    // Initialise the first and second carousel by class selector.
+	// Note that they use both the same configuration options (none in this case).
+	jQuery('.d-carousel .carousel').jcarousel({
+        scroll: 1
+    });
+});
